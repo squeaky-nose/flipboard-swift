b7ffe73678ead872e11f3ad29d2750ec1abdf72f
Sushant Verma <sushant.40@gmail.com>
2025-10-22 23:35:58 +1100

    Initial project

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "flipboard-swift",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "flipboard-swift",
+            targets: ["flipboard-swift"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "flipboard-swift"
+        ),
+        .testTarget(
+            name: "flipboard-swiftTests",
+            dependencies: ["flipboard-swift"]
+        ),
+    ]
+)
